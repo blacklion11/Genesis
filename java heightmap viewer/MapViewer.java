1843ab539adb46350ce27e6db92272939d2b5539
unknown <MELLOMJ-WKS@HOUSNG2663272.offices.uwec.edu>
2015-12-15 15:55:04 -0600

    update

--- java heightmap viewer/MapViewer.java
+++ java heightmap viewer/MapViewer.java
@@ -2,6 +2,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
+import java.util.ArrayList;
 
 public class MapViewer extends JFrame
 {
@@ -10,6 +11,7 @@ public class MapViewer extends JFrame
 	int width;
 	int height;
 	int[][] map;
+	ArrayList<String> lines;
 	
 	
 	public MapViewer()
@@ -26,22 +28,56 @@ public class MapViewer extends JFrame
 		this();
 		
 		readFile(filename);
+		parseLines();
+		
+		for(int i = 0; i < map.length; i++)
+		{
+			for(int j = 0; j < map[0].length; j++)
+			{
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
 	}
 	
-	public void readFile(String filename)
+	void readFile(String filename)
 	{
+		lines = new ArrayList<String>();
+		
 		try
 		{
 			br = new BufferedReader(new FileReader(filename));
 			String line = br.readLine();
 			while(line != null)
 			{
-				System.out.println(line);
+				//System.out.println(line);
+				lines.add(line);
 				
 				line = br.readLine();
 			}
 		}
 		catch(IOException e){e.printStackTrace();}
+	}
+	
+	void parseLines()
+	{
+		map = new int[lines.size()][];
+		for(int i = 0; i < lines.size(); i++)
+		{
+			String[] tokens = lines.get(i).split(" ");
+			if(i == 0)
+			{
+				for(int k = 0; k < map.length; k++)
+				{
+					map[k] = new int[tokens.length];
+				}
+			}
+			
+			for(int j = 0; j < tokens.length; j++)
+			{
+				map[i][j] = Integer.parseInt(tokens[j]);
+			}
+		}
 	}
 	
 	
